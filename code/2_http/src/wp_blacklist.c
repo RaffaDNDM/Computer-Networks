ee3893d1d320cfbc4b82f4bbb5592e3a4583cf5e
RaffaDNDM <dinardodimaioraffaele@gmail.com>
2020-07-17 15:23:03 +0200

    Updated proxy with management of blacklist.

--- code/2_http/src/wp_blacklist.c
+++ code/2_http/src/wp_blacklist.c
@@ -10,16 +10,15 @@
 #include <stdlib.h>
 #include <netdb.h>
 
-#define NUM_BLOCKED_IP 4
+#define NUM_BLOCKED_IP 3
 
 struct hostent * he;
 struct sockaddr_in local,remote,server;
 char request[10000],response[2000],request2[2000],response2[2000];
 char * method, *path, *version, *host, *scheme, *resource,*port;
-char blocked_IPs[NUM_BLOCKED_IP][4] = {{192,168,1,81},
-                                       {192,168,1,210},
-                                       {192,168, 1,1},
-                                       {192,165,22,1}};
+char blocked_IPs[NUM_BLOCKED_IP][20] = {"www.google.com",
+                                       "www.radioamatori.it",
+                                       "www.youtube.it"};
 
 struct headers {
     char *n;
@@ -116,14 +115,23 @@ int main()
 		
         if(!strcmp("GET",method))
         {
-            //  http://www.google.com/path
+            // http://www.google.com/path
 			scheme=path;
 			for(i=0;path[i]!=':';i++); path[i]=0;
 			host=path+i+3; 
 			for(i=i+3;path[i]!='/';i++); path[i]=0;
 			resource=path+i+1;
 			printf("Scheme=%s, host=%s, resource = %s\n", scheme,host,resource);
-			
+		
+            for(i=0; i<NUM_BLOCKED_IP; i++)
+            {
+                if(!strcmp(host, blocked_IPs[i]))
+                {
+                    block=1;
+                    break;
+                }
+            }
+
             he = gethostbyname(host);
 			if (he == NULL) 
             { 
@@ -148,10 +156,7 @@ int main()
                 perror("Connect to server failed"); 
                 return 1;
             }
-
-			sprintf(request2,"GET /%s HTTP/1.1\r\nHost:%s\r\nConnection:close\r\n\r\n",resource,host);
-            write(s3,request2,strlen(request2)); 
-			
+            
             if(block)
             {
                 sprintf(response2, "HTTP/1.1 401 Unauthorized\r\n\r\n");
@@ -159,17 +164,8 @@ int main()
             }
             else
             {
-                sprintf(response2, "%s\r\n", h[0].n);
-                write(s2, response2, strlen(response2));
-
-                for(i=1; h[i].n[0]; i++)
-                {
-                    sprintf(response2, "%s:%s\r\n", h[i].n, h[i].v);
-                    write(s2, response2, strlen(response2));
-                }
-
-                sprintf(response2, "\r\n");
-                write(s2, response2, 2);
+			    sprintf(request2,"GET /%s HTTP/1.1\r\nHost:%s\r\nConnection:close\r\n\r\n",resource,host);
+                write(s3,request2,strlen(request2)); 
 
                 while((t=read(s3, response2, 2000))>0)
                     write(s2, response2, t);
@@ -185,63 +181,82 @@ int main()
             close(s3);
 		}
 		else if(!strcmp("CONNECT",method)) 
-        { 
+        {
+            // www.google.com:400 
 			host=path;
 			for(i=0;path[i]!=':';i++); path[i]=0;
 			port=path+i+1;
 			printf("host:%s, port:%s\n",host,port);
-			printf("Connect skipped ...\n");	
-			he = gethostbyname(host);
-			if (he == NULL) 
-            { 
-                printf("Gethostbyname Failed\n"); 
-                return 1;
+
+            for(i=0; i<NUM_BLOCKED_IP; i++)
+            {
+                if(!strcmp(host, blocked_IPs[i]))
+                {
+                    block=1;
+                    break;
+                }
             }
 
-			printf("Connecting to address = %u.%u.%u.%u\n", (unsigned char ) he->h_addr[0],(unsigned char ) he->h_addr[1],(unsigned char ) he->h_addr[2],(unsigned char ) he->h_addr[3]); 			
-			s3=socket(AF_INET,SOCK_STREAM,0);
-			if(s3==-1)
+            if(block)
             {
-                perror("Socket to server failed"); 
-                return 1;
+                sprintf(response2, "HTTP/1.1 401 Unauthorized\r\n\r\n");
+                write(s2, response2, strlen(response2));
             }
+            else
+            {
+                he = gethostbyname(host);
+                if (he == NULL) 
+                { 
+                    printf("Gethostbyname Failed\n"); 
+                    return 1;
+                }
 
-			server.sin_family=AF_INET;
-			server.sin_port=htons((unsigned short)atoi(port));
-		 	server.sin_addr.s_addr=*(unsigned int*) he->h_addr;			
-			t=connect(s3,(struct sockaddr *)&server,sizeof(struct sockaddr_in));		
-            if(t==-1)
-            {
-                perror("Connect to server failed"); 
-                exit(0);
-            }
-			
-            sprintf(response,"HTTP/1.1 200 Established\r\n\r\n");
-			write(s2,response,strlen(response));
-			
-            if(!(pid=fork())) //Child
-            { 
-				while(t=read(s2,request2,2000))
-                {	
-                    write(s3,request2,t);
-					printf("CL >>>(%d)%s \n",t,host); //SOLO PER CHECK
-                }	
-				exit(0);
-            }
-			else //Parent	
-            { 
-				while(t=read(s3,response2,2000))
-                {	
-					write(s2,response2,t);
-					printf("CL <<<(%d)%s \n",t,host);
-                }	
-				
-                kill(pid,15);
-				shutdown(s3,SHUT_RDWR);
-				close(s3);
-            }	
+                printf("Connecting to address = %u.%u.%u.%u\n", (unsigned char ) he->h_addr[0],(unsigned char ) he->h_addr[1],(unsigned char ) he->h_addr[2],(unsigned char ) he->h_addr[3]); 			
+                s3=socket(AF_INET,SOCK_STREAM,0);
+                if(s3==-1)
+                {
+                    perror("Socket to server failed"); 
+                    return 1;
+                }
+
+                server.sin_family=AF_INET;
+                server.sin_port=htons((unsigned short)atoi(port));
+                server.sin_addr.s_addr=*(unsigned int*) he->h_addr;			
+                t=connect(s3,(struct sockaddr *)&server,sizeof(struct sockaddr_in));		
+                if(t==-1)
+                {
+                    perror("Connect to server failed"); 
+                    exit(0);
+                }
+                
+                sprintf(response,"HTTP/1.1 200 Established\r\n\r\n");
+                write(s2,response,strlen(response));
+                
+                if(!(pid=fork())) //Child
+                { 
+                    while(t=read(s2,request2,2000))
+                    {	
+                        write(s3,request2,t);
+                        printf("CL >>>(%d)%s \n",t,host); //SOLO PER CHECK
+                    }	
+                    exit(0);
+                }
+                else //Parent	
+                { 
+                    while(t=read(s3,response2,2000))
+                    {	
+                        write(s2,response2,t);
+                        printf("CL <<<(%d)%s \n",t,host);
+                    }	
+                    
+                    kill(pid,15);
+                    shutdown(s3,SHUT_RDWR);
+                    close(s3);
+                }
+            }    
 		}	
-		shutdown(s2,SHUT_RDWR);
+		
+        shutdown(s2,SHUT_RDWR);
 		close(s2);	
 		exit(0);
 	}
