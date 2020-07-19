6defc145312b59d55b69633b5dddcdb1c601e658
RaffaDNDM <dinardodimaioraffaele@gmail.com>
2020-07-19 19:47:00 +0200

    Added new version of program for bitrate limit with bug.

--- code/2_http/src/wp_limit_avg_bitrate2.c
+++ code/2_http/src/wp_limit_avg_bitrate2.c
@@ -27,7 +27,7 @@ int main()
 {
     FILE *f;
     char command[100];
-    int i,s,t,s2,s3,n,len,c,yes=1,j,k,pid; 
+    int i,s,t,s2,s3,n,len,c,yes=1,j,k,pid, size; 
     
     s = socket(AF_INET, SOCK_STREAM, 0);
     if ( s == -1) 
@@ -180,135 +180,136 @@ int main()
 			
             if(!(pid=fork())) //Child
             { 
-                struct timeval t1;
-                struct timeval t2;
-                suseconds_t diff_sec, diff_usec, estimated_sec, estimated_usec;
-
-                if(gettimeofday(&t1, NULL))
+                t=1;
+                while(t)
                 {
-                    printf("[PROXY ERROR] gettimeofday\n");
-                    exit(1);
-                }
-				
-                while(t=read(s2,request2,2000))
-                {	
-					write(s3,request2,t);
-                    
-                    if(gettimeofday(&t2, NULL))
-                    {
-                        printf("[PROXY ERROR] gettimeofday\n");
-                        exit(1);
-                    }
-					
-                    estimated_usec = t*8000;
-                    diff_sec = t2.tv_sec - t1.tv_sec;
-                    
-                    if(t2.tv_usec>t1.tv_usec)
-                        diff_usec = t2.tv_usec - t1.tv_usec;
-                    else
-                    {
-                        diff_usec = t2.tv_usec +1000000 - t1.tv_usec;
-                        diff_sec=(diff_sec>0)?diff_sec-1:diff_sec;
-                    }
+                    struct timeval t1, t2;
+                    time_t diff_sec;
+                    suseconds_t estimated_usec, diff_usec;
 
-                    if((diff_sec*1000000+diff_usec)<estimated_usec)
-                        usleep(estimated_usec-diff_sec*1000000-diff_usec);
-
-                    if(gettimeofday(&t2, NULL))
-                    {
-                        printf("[PROXY ERROR] gettimeofday\n");
-                        exit(1);
-                    }
-
-                    diff_sec = t2.tv_sec - t1.tv_sec;
-                    
-                    if(t2.tv_usec>t1.tv_usec)
-                        diff_usec = t2.tv_usec - t1.tv_usec;
-                    else
-                    {
-                        diff_usec = t2.tv_usec +1000000 - t1.tv_usec;
-                        diff_sec=(diff_sec>0)?diff_sec-1:diff_sec;
-                    }
-
-                    printf("%sBitrate:%s  %6.3lf Kbit/s    ", BOLD_RED, DEFAULT, ((double) (t*8*1000))/(diff_sec*1000000.0+diff_usec));
-					printf("%sC >>> S(%s%4d%s): %s%s%s \n",BOLD_RED, DEFAULT, t, BOLD_RED, BOLD_YELLOW, host, DEFAULT);
-
-                    //To be more accurate in the next evaluation
                     if(gettimeofday(&t1, NULL))
                     {
                         printf("[PROXY ERROR] gettimeofday\n");
                         exit(1);
                     }
-                }	
+
+                    for(size=0;(t=read(s2,request2,125-size)); size+=t)
+                        write(s3,request2,t);
+
+                    if(t==-1)
+                    {
+                        printf("[PROXY ERROR] Reading\n");
+                        exit(1);
+                    }
+                        if(gettimeofday(&t2, NULL))
+                        {
+                            printf("[PROXY ERROR] gettimeofday\n");
+                            exit(1);
+                        }
+                        
+                        estimated_usec = size*8000;
+                        diff_sec = t2.tv_sec - t1.tv_sec;
+                        
+                        if(t2.tv_usec>t1.tv_usec)
+                            diff_usec = t2.tv_usec - t1.tv_usec;
+                        else
+                        {
+                            diff_usec = t2.tv_usec +1000000 - t1.tv_usec;
+                            diff_sec=(diff_sec>0)?diff_sec-1:diff_sec;
+                        }
+
+                        if((diff_sec*1000000+diff_usec)<estimated_usec)
+                            usleep(estimated_usec-diff_sec*1000000-diff_usec);
+
+                        if(gettimeofday(&t2, NULL))
+                        {
+                            printf("[PROXY ERROR] gettimeofday\n");
+                            exit(1);
+                        }
+
+                        diff_sec = t2.tv_sec - t1.tv_sec;
+                        
+                        if(t2.tv_usec>t1.tv_usec)
+                            diff_usec = t2.tv_usec - t1.tv_usec;
+                        else
+                        {
+                            diff_usec = t2.tv_usec +1000000 - t1.tv_usec;
+                            diff_sec=(diff_sec>0)?diff_sec-1:diff_sec;
+                        }
+
+                        printf("%sBitrate:%s  %6.3lf Kbit/s    ", BOLD_RED, DEFAULT, ((double) (size*8*1000))/(diff_sec*1000000.0+diff_usec));
+                        printf("%sC >>> S(%s%4d%s): %s%s%s \n",BOLD_RED, DEFAULT, size, BOLD_RED, BOLD_YELLOW, host, DEFAULT);
+                }
 				
                 exit(0);
             }
 			else //Parent	
             { 
-                struct timeval t1;
-                struct timeval t2;
-                suseconds_t diff_sec, diff_usec, estimated_sec, estimated_usec;
-                
-                if(gettimeofday(&t1, NULL))
+                t=1;
+                while(t)
                 {
-                    printf("[PROXY ERROR] gettimeofday\n");
-                    exit(1);
-                }
+                    struct timeval t1, t2;
+                    time_t diff_sec;
+                    suseconds_t estimated_usec, diff_usec;
 
-				while(t=read(s3,response2,2000))
-                {	
-					write(s2,response2,t);
-                    
-                    if(gettimeofday(&t2, NULL))
-                    {
-                        printf("[PROXY ERROR] gettimeofday\n");
-                        exit(1);
-                    }
-					
-                    estimated_usec = t*800;
-                    diff_sec = t2.tv_sec - t1.tv_sec;
-                    
-                    if(t2.tv_usec>t1.tv_usec)
-                        diff_usec = t2.tv_usec - t1.tv_usec;
-                    else
-                    {
-                        diff_usec = t2.tv_usec +1000000 - t1.tv_usec;
-                        diff_sec=(diff_sec>0)?diff_sec-1:diff_sec;
-                    }
-
-                    if((diff_sec*1000000+diff_usec)<estimated_usec)
-                        usleep(estimated_usec-diff_sec*1000000-diff_usec);
-                    
-                    if(gettimeofday(&t2, NULL))
-                    {
-                        printf("[PROXY ERROR] gettimeofday\n");
-                        exit(1);
-                    }
-
-                    diff_sec = t2.tv_sec - t1.tv_sec;
-                    
-                    if(t2.tv_usec>t1.tv_usec)
-                        diff_usec = t2.tv_usec - t1.tv_usec;
-                    else
-                    {
-                        diff_usec = t2.tv_usec +1000000 - t1.tv_usec;
-                        diff_sec=(diff_sec>0)?diff_sec-1:diff_sec;
-                    }
-
-                    printf("%sBitrate:%s  %6.3lf Kbit/s    ", BOLD_BLUE, DEFAULT, (((double) t)*8*1000)/(diff_sec*1000000.0+diff_usec));
-					printf("%sC <<< S(%s%4d%s): %s%s%s \n",BOLD_BLUE, DEFAULT, t, BOLD_BLUE, BOLD_CYAN, host, DEFAULT);
-                    
-                    //To be more accurate in the next evaluation
                     if(gettimeofday(&t1, NULL))
                     {
                         printf("[PROXY ERROR] gettimeofday\n");
                         exit(1);
                     }
-                }	
-				
-                kill(pid,15);
-				shutdown(s3,SHUT_RDWR);
-				close(s3);
+
+                    for(size=0;(t=read(s3,response2,1250-size)); size+=t)
+                        write(s2,response2,t);
+
+                    if(t==-1)
+                    {
+                        printf("[PROXY ERROR] Reading\n");
+                        exit(1);
+                    }
+                        if(gettimeofday(&t2, NULL))
+                        {
+                            printf("[PROXY ERROR] gettimeofday\n");
+                            exit(1);
+                        }
+                        
+                        estimated_usec = size*800;
+                        printf("%d\n", estimated_usec);
+                        diff_sec = t2.tv_sec - t1.tv_sec;
+                        
+                        if(t2.tv_usec>t1.tv_usec)
+                            diff_usec = t2.tv_usec - t1.tv_usec;
+                        else
+                        {
+                            diff_usec = t2.tv_usec +1000000 - t1.tv_usec;
+                            diff_sec=(diff_sec>0)?diff_sec-1:diff_sec;
+                        }
+
+                        if((diff_sec*1000000+diff_usec)<estimated_usec)
+                            usleep(estimated_usec-diff_sec*1000000-diff_usec);
+
+                        if(gettimeofday(&t2, NULL))
+                        {
+                            printf("[PROXY ERROR] gettimeofday\n");
+                            exit(1);
+                        }
+
+                        diff_sec = t2.tv_sec - t1.tv_sec;
+                        
+                        if(t2.tv_usec>t1.tv_usec)
+                            diff_usec = t2.tv_usec - t1.tv_usec;
+                        else
+                        {
+                            diff_usec = t2.tv_usec +1000000 - t1.tv_usec;
+                            diff_sec=(diff_sec>0)?diff_sec-1:diff_sec;
+                        }
+
+                        printf("%sBitrate:%s  %6.3lf Kbit/s    ", BOLD_BLUE, DEFAULT, ((double) (size*8*1000))/(diff_sec*1000000.0+diff_usec));
+                        printf("%sC <<< S(%s%4d%s): %s%s%s \n",BOLD_BLUE, DEFAULT, size, BOLD_BLUE, BOLD_CYAN, host, DEFAULT);
+			    }
+
+                kill(pid, 15);
+	            shutdown(s3,SHUT_RDWR);
+                close(s3);
             }	
 		}
 
