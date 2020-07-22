1672ecea6233669a58e864797268275b7fb84b26
RaffaDNDM <dinardodimaioraffaele@gmail.com>
2020-07-22 23:58:23 +0200

    Updated Web Server and Web Client in python.

--- code/2_http/src/wc.py
+++ code/2_http/src/wc.py
@@ -1,29 +1,49 @@
-import socket
 import sys
+import socket
 
-#Remote machine
-address="www.google.com"
-port = 80
+IP_ADDRESS = '192.168.1.81'
+PORT = 80
 
-if(len(sys.argv)==2):
-    address, port = sys.argv
+def connection(IP_ADDRESS, PORT):
+    if(len(sys.argv)==3):
+        IP_ADDRESS = sys.argv[1]
+        PORT = int(sys.argv[2])
 
-#Definition of the socket
-sd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sd.connect((IP_ADDRESS, PORT))
 
-#Connection to remote server
-sd.connect((address, port))
+    return sd
 
-#Writing the HTTP/1.1 request
-request = 'GET / HTTP/1.0\r\nHost: '+address+'\r\n\r\n'
-sd.send(request.encode()) #to convert in byte notation
 
-#Reading the response (concatenation of received messages)
-response = sd.recv(1024)
+def request(sd):
+    cmd = 'GET /prova.html HTTP/1.1\r\nHost: www.google.com\r\n\r\n'
+    sd.send(cmd.encode())    
 
-while len(response)>0:
-    print(response)
-    response = sd.recv(1024)
 
-#Closing the socket
-sd.close()
+def response(sd):
+    response=''
+    while(True):
+        data = sd.recv(512)
+        if len(data)<1:
+            break
+        response+=data.decode('utf-8', 'ignore')
+        
+    parts_message = response.split('\r\n\r\n')
+    headers = parts_message[0].split('\r\n')
+    print(headers)
+    request_line = headers[0].split(' ')
+    print(request_line)
+    d = dict(x.split(":") for x in headers[1:])
+    print(d)
+    print(parts_message[1])
+
+
+def main():
+    sd = connection(IP_ADDRESS, PORT)
+    request(sd)
+    response(sd)
+    sd.close()
+
+if __name__ == "__main__":
+    print(sys.argv)
+    main()
--- code/2_http/src/ws.py
+++ code/2_http/src/ws.py
@@ -1,27 +1,76 @@
+import sys
 import socket
 import threading
+import os
 
-port = 8080
+PORT = 8080
 
-if(len(sys.argv)==1)
-    port = sys.argv
+def create_server(PORT):
+    if(len(sys.argv)==2):
+        PORT = int(sys.argv[1])
 
-server = socket.gethostbyname(socket.gethostname())
+    sd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-sd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((server, port))
+    try:
+        sd.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sd.bind(('', PORT))
+        sd.listen(5)
+    except KeyboardInterrupt:
+        print("Shutdown\n")
+    except Exception as e:
+        print(e)
 
-#Management of a single client request
-def manage_client(conn, addr):
-    msg = conn.recv(1024)
+    return sd
 
-def start_server():
-    sd.listen()
 
-    while True:
-        conn, addr = sd.accept()
-        thread = threading.Thread(target=manage_client, args=(conn, addr))
-        thread.start()
-        print(f"[CONNECTED CLIENTS] {threading.activeCount()-1}")
+def handle_client(sd, addr):
+    print("Request from "+str(addr))
+    request = sd.recv(2000).decode('utf-8', 'ignore')
 
-print
+    #Parser of request)
+    try:
+        headers = request.split('\r\n')
+        print(headers)
+        request_line = headers[0].split(' ')
+        print(request_line)
+        d = dict(x.split(":") for x in headers[1:-2])
+        print(d)
+
+        if os.path.exists(request_line[1][1:]):
+            status_line = "HTTP/1.1 200 OK\r\n"
+            sd.send(status_line.encode())
+
+            with open(request_line[1][1:], "r+") as f:
+                x=f.read().encode()
+            
+            y="Content-Length: "+str(len(x))+"\r\n\r\n"
+            sd.send(y.encode())
+            sd.send(x)
+
+        else:
+            status_line = "HTTP/1.1 404 Not Found\r\n\r\n"
+            sd.send(status_line.encode())
+
+        sd.close()
+
+    except KeyboardInterrupt:
+        print("Shutdown\n")
+    except Exception as e:
+        print(e)
+
+
+def start_server(sd):
+    while(1):
+        client_sd, addr = sd.accept()
+        print("ciao")
+        cl = threading.Thread(target=handle_client, args=(client_sd, addr))
+        cl.start()
+
+
+def main():
+    sd = create_server(PORT)
+    start_server(sd)
+
+
+if __name__ == "__main__":
+    main()
